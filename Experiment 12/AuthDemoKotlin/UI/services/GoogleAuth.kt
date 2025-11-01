0926fabf2218edfbdbc0a7268528db9c1052aab8
AnuragBorker <anuragborker726@gmail.com>
2025-11-01 22:39:11 +0530

    Add files via upload

---
+++ Experiment 12/AuthDemoKotlin/UI/services/GoogleAuth.kt
@@ -0,0 +1,15 @@
+package com.example.authdemokotlin.ui.services
+
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.example.authdemokotlin.R
+
+fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(context.getString(R.string.default_web_client_id))
+        .requestEmail()
+        .build()
+    return GoogleSignIn.getClient(context, gso)
+}
